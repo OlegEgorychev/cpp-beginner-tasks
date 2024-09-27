1682bad159c17268f34e2dd27e5d616d6a8a7aaf
Oleg Egorychev <egorychevoleg92@gmail.com>
2024-09-27 09:19:43 +0400

    Add project files.

---
+++ C++ Beginner/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+	cout << "Task 1: Even or Odd Number" << endl << "Description: The user inputs a number.Determine if it is even or odd." << endl << endl;
+	int a;
+	char choice;
+	
+	do {
+		cout << "Please, enter a number " << endl;
+		cin >> a;
+
+		if (a % 2 == 0) {
+			cout << "It's an even number." << endl;
+		}
+
+		else {
+			cout << "It's an odd number." << endl;
+		}
+
+		cout << "Whould you like to try again? (use y/Y)" << endl;
+		cin >> choice;
+
+	} 
+		while (choice == 'y' || choice == 'Y');
+		cout << "Thank you for using the programm" << endl;
+}
