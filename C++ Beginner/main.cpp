a46da8947136f013a7fa1440e993ff9844ccc92b
Oleg Egorychev <egorychevoleg92@gmail.com>
2024-10-02 09:07:08 +0400

    Task 3: Find the Maximum Element in an Array - in progress

--- C++ Beginner/main.cpp
+++ C++ Beginner/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void task1() {
-	cout << "Task 1: Even or Odd Number" << endl << "Description: The user inputs a number.Determine if it is even or odd." << endl << endl;
+	cout << "Task 1: Even or Odd Number" << endl << "Description: The user inputs a number. Determine if it is even or odd." << endl << endl;
 	int a;
 	char choice;
 
@@ -26,7 +26,25 @@ void task1() {
 }
 
 void task2() {
-	cout << "Bye, World!";
+	cout << "Task 2: Sum of Numbers from 1 to N" << endl << "Description : The user inputs a number N. Calculate the sum of all numbers from 1 to N inclusive." << endl << endl;
+	int a;
+	cin >> a;
+
+	int sum = 0;
+	for (int i = 1; i <= a; i++) {
+		sum += i;
+	}
+	cout << sum;
+}
+
+void task3() {
+	cout << "Task 3: Find the Maximum Element in an Array" << endl << "Description: Given N integers in an array. Find the maximum number in the array." << endl;
+	int numbers[3];
+	for (int i = 0; i <= 4; i++) {
+		cout << "Enter elevent " << i + 1 << ": ";
+		cin >> numbers[i];
+	}
+	
 }
 
 int main() {
@@ -40,6 +58,9 @@ int main() {
 			break;
 		case 2:
 			task2();
+			break;
+		case 3:
+			task3();
 			break;
 	}
 }
