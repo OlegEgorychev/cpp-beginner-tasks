1082013038c985e8b6691a384b7bbc73d9ec242d
Oleg Egorychev <egorychevoleg92@gmail.com>
2024-10-08 10:48:58 +0400

    added menu for showing all tasks

--- C++ Beginner/main.cpp
+++ C++ Beginner/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>;
 using namespace std;
 
+string taskTitles[]{
+	"Task 1: Even or Odd Number",
+	"Task 2: Sum of Numbers from 1 to N",
+	"Task 3: Find the Maximum Element in an Array - IN PROGRESS",
+	"Task 4: Count Positive Numbers in an Array",
+	"Task 5: Sum of Array Elements",
+	"Task 6: Simple Calculator",
+	"Task 7: Multiplication Table",
+	"Task 8: Reverse an Array",
+	"Task 9: Count Even Numbers in an Array",
+	"Task 10: Minimum of Two Numbers",
+	"Task 11: Convert Centimeters to Inches",
+};
+
+string taskDescription[] {
+	/* Task 1 */ "The user inputs a number. Determine if it is even or odd.",
+	/* Task 2 */ "The user inputs a number N. Calculate the sum of all numbers from 1 to N inclusive.",
+	/* Task 3 */ "Given N integers in an array. Find the maximum number in the array.",
+	/* Task 4 */ "The user inputs N numbers. Determine how many of them are positive.",
+	/* Task 5 */ "Given N numbers in an array. Find the sum of all the elements in the array.",
+	/* Task 6 */ "The user inputs two numbers and an operation symbol (+, -, *, /). Output the result of the corresponding operation.",
+	/* Task 7 */ "Output the multiplication table for a number input by the user (from 1 to 10).",
+	/* Task 8 */ "The user inputs N numbers. Output them in reverse order.",
+	/* Task 9 */ "The user inputs an array of numbers. Find and output the count of even numbers in the array.",
+	/* Task 10 */"The user inputs two numbers. Output the minimum of the two.",
+	/* Task 11 */"The user inputs a length in centimeters. The program should convert it into inches, knowing that 1 inch equals 2.54 centimeters. The input value can be a decimal number."
+
+};
+
+void displayTaskHeader(int taskNumber) {
+	cout << taskTitles[taskNumber] << endl;
+	cout << "Description: " << taskDescription[taskNumber] << endl << endl;
+}
+
+void displayAvailableTasks() {
+	cout << "Available tasks: " << endl;
+	for (int i = 0; i <= 11; i++) {
+		cout << i + 1 << ". " << taskTitles[i] << endl;
+	}
+	cout << endl;
+}
+
 void task1() {
-	cout << "Task 1: Even or Odd Number" << endl << "Description: The user inputs a number. Determine if it is even or odd." << endl << endl;
+	displayTaskHeader(0);
 	int a;
 	char choice;
 
@@ -26,7 +68,7 @@ void task1() {
 }
 
 void task2() {
-	cout << "Task 2: Sum of Numbers from 1 to N" << endl << "Description : The user inputs a number N. Calculate the sum of all numbers from 1 to N inclusive." << endl << endl;
+	displayTaskHeader(1);
 	int a;
 	cin >> a;
 
@@ -38,20 +80,31 @@ void task2() {
 }
 
 void task3() {
-	cout << "Task 3: Find the Maximum Element in an Array" << endl << "Description: Given N integers in an array. Find the maximum number in the array." << endl;
-	int numbers[3]{};
+	displayTaskHeader(2);
+	int numbers[3]{ 0, 1, 10 };
 	
 	for (int i = 0; i <= 2; i++) {
 		cout << "Enter array's element " << i + 1 << ": " << endl;
 		cin >> numbers[i];
 	}
 		
-		cout << numbers[0,1,2] << endl;
+		cout << numbers[0,1,2] << endl;}
+
+void task4() {
+	displayTaskHeader(3);
+	double centimeters;
+	cout << "Enter the length in centimeters to find out the length in inches: ";
+	cin >> centimeters;
+	const double inches_per_centimeter = 2.54;
+	cout << "Length in inches = " << centimeters / inches_per_centimeter;
 
 }
 
 int main() {
 	int choice;
+
+	displayAvailableTasks();
+
 	cout << "Choice task:";
 	cin >> choice;
 
@@ -64,6 +117,9 @@ int main() {
 			break;
 		case 3:
 			task3();
+			break;
+		case 4:
+			task4();
 			break;
 	}
 }
